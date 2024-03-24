eb24df6c905e77b88b9278980883b622069bba79
ChiThang-50Cent <chithang.nydo@gmail.com>
2024-03-24 23:06:05 +0700

    update

--- models.py
+++ models.py
@@ -4,8 +4,7 @@ import torchvision
 
 class Models:
     def __init__(
-        self, model_name, num_classes, 
-        feature_extract, pre_trained=True
+        self, model_name, num_classes, feature_extract, pre_trained=True
     ) -> None:
         self.model_name = model_name
         self.pre_trained = pre_trained
@@ -27,7 +26,13 @@ class Models:
             model.classifier[-1] = torch.nn.Linear(num_ftrs, self.num_classes)
 
         elif self.model_name == "mobilenetv3":
-            model = torchvision.models.mobilenet_v3_large(pretrained=self.pre_trained)
+            weight = (
+                None
+                if not self.pre_trained
+                else torchvision.models.MobileNet_V3_Large_Weights
+            )
+
+            model = torchvision.models.mobilenet_v3_large(weight=weight)
             self.set_parameter_requires_grad(model)
             num_ftrs = model.classifier[-1].in_features
             model.classifier[-1] = torch.nn.Linear(num_ftrs, self.num_classes)
