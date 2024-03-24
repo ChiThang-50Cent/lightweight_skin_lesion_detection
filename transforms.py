799caf68663088d0cf7fce1355277cce3c3e75a7
ChiThang-50Cent <chithang.nydo@gmail.com>
2024-03-24 22:13:10 +0700

    update

--- transforms.py
+++ transforms.py
@@ -1,3 +1,4 @@
+import torch
 from torchvision.transforms import v2
 
 
@@ -5,12 +6,13 @@ def get_train_transform(size):
 
     transform = v2.Compose(
         [
+            v2.ToImage(), 
             v2.Resize((size, size)),
             v2.RandomHorizontalFlip(),
             v2.RandomVerticalFlip(),
             v2.RandomRotation(20),
             v2.ColorJitter(brightness=0.1, contrast=0.1, hue=0.1),
-            v2.ToTensor(),
+            v2.ToDtype(torch.float32, scale=True),
             v2.Normalize(
                 mean=[0.763038, 0.54564667, 0.57004464],
                 std=[0.14092727, 0.15261286, 0.1699712],
@@ -25,8 +27,9 @@ def get_val_transform(size):
 
     val_transform = v2.Compose(
         [
+            v2.ToImage(), 
             v2.Resize((size, size)),
-            v2.ToTensor(),
+            v2.ToDtype(torch.float32, scale=True),
             v2.Normalize(
                 mean=[0.763038, 0.54564667, 0.57004464],
                 std=[0.14092727, 0.15261286, 0.1699712],
