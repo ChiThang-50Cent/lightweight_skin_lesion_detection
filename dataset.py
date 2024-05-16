fb249948b093a9f2c36778e9b3d8c389c5e9acaa
ChiThang-50Cent <chithang.nydo@gmail.com>
2024-05-16 23:20:17 +0700

    add preprocessing img

--- dataset.py
+++ dataset.py
@@ -1,5 +1,7 @@
 import os
+import cv2
 import torch
+import numpy as np
 
 from PIL import Image
 from torch.utils.data import Dataset
@@ -10,6 +12,8 @@ class Custom_Dataset(Dataset):
         self.df = df
         self.root_dir = root_dir
         self.transform = transform
+        self.kernel = cv2.getStructuringElement(1,(17,17))
+
 
     def __len__(self):
         return len(self.df)
@@ -18,10 +22,18 @@ class Custom_Dataset(Dataset):
 
         img_path = os.path.join(self.root_dir, f"{self.df['image_id'][index]}.{'jpg'}")
         img = Image.open(img_path)
+         
+        img = np.array(img)
+        grayScale = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY ) #1 Convert the original image to grayscale
+        blackhat = cv2.morphologyEx(grayScale, cv2.MORPH_BLACKHAT, self.kernel) #2 Perform the blackHat filtering on the grayscale image to find the hair countours
+        _,thresh2 = cv2.threshold(blackhat,10,255,cv2.THRESH_BINARY) # intensify the hair countours in preparation for the inpainting algorithm
+        dst = cv2.inpaint(img,thresh2,1,cv2.INPAINT_TELEA) # inpaint the original image depending on the mask
+        
+        img_new = Image.fromarray(dst)
 
         label = torch.tensor(int(self.df["encoded_dx"][index]))
 
         if self.transform:
-            img = self.transform(img)
+            img_new = self.transform(img_new)
 
-        return img, label
+        return img_new, label
